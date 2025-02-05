2ce08149fadc91c8bbeb0e5304d3bade3ec705ec
john <john@doe>
2025-02-05 22:02:12 +0900

    URL process.env

--- src/pages/api/ws.js
+++ src/pages/api/ws.js
@@ -187,7 +187,7 @@ const handler = (req, res) => {
             clientManager = new ClientManager();
 
             // Initialize GameClientManager
-            const GAME_SERVER_URL = `${process.env.HOLDEM_WS}`;//'wss://dev-holdem.emp555.com';//'ws://178.128.17.145:4000';
+            const GAME_SERVER_URL = 'wss://holdem.emp555.com';//${process.env.HOLDEM_WS}`;//'wss://dev-holdem.emp555.com';//'ws://178.128.17.145:4000';
             gameClientManager = new GameClientManager(
                 GAME_SERVER_URL,
                 { id: 'a', password: 'a' },
